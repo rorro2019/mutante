fc30c72636348dda9538dc4bd863327e6e3c0157
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2022-05-23 20:29:46 -0300

    primer commit

---
+++ src/test/java/com/mutante/mutante/MutanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutante.mutante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
